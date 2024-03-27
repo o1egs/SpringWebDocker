796070853fda2ba6ae222827e7223ec0dcfc5961
o1egs <tankistonline21@gmail.com>
2024-03-27 10:49:47 +0300

    Initial commit

---
+++ src/test/java/ru/shtyrev/SpringWebDocker/SpringWebDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shtyrev.SpringWebDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
